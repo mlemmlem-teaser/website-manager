5e731ddd3c8e731b49522bb592ba007912707f1a
mlemmlem-teaser <masteryibm@gmail.com>
2024-03-26 11:09:19 +0700

    fsd

--- src/screens/client/Auth/Register/register.js
+++ src/screens/client/Auth/Register/register.js
@@ -1,12 +1,22 @@
-// import {app} from "../../../../config-firebase.js";
-// console.log(app.name);
-const Username =document.getElementById("form2Example3");
-const Email =document.getElementById("form2Example1");
-const Password =document.getElementById("form2Example2");
-const date =document.getElementById("form2Example4");
 
-function Emailcheck() {
-    if(Email.value==""||Email.value.length<=10) {
+import { app } from "../../../../config-firebase.js";
+import { Auth } from "../../../../config-firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { dbFireStore } from "../../../../config-firebase.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+const form = document.querySelector(".form_login");
+ async function registerUser(e) {
+ e.preventDefault();
+ const { target } = e;
+ const email = target.email.value;
+ const password =target.password.value;
+ const username = target.username.value;
+ const date = target.date.value;
+ const gender = target.gender.value;
+ console.log(email,password,username,date,gender);
+
+ function Emailcheck() {
+    if(email==""||email.length<=10) {
         alert("Email không hợp lệ");
         return false
     } else {
@@ -14,22 +24,11 @@ function Emailcheck() {
     }
 }
 function Gendercheck() {
-    const male= document.getElementById("male");
-    const female= document.getElementById("female");
-    const other= document.getElementById("other");
-    if (male.checked) {
-        return male.value;
-    }
-    if (female.checked) {
-        return female.value;
-    }
-    if (other.checked) {
-        return other.value;
-    }
+    return gender;
 };
 
 function Usernamecheck() { 
-    if (Username.value.length<=6||["0","1","2","3","4","5","6","7","8","9"].includes((Username.value).charAt(0))) {
+    if (username.length<=6||["0","1","2","3","4","5","6","7","8","9"].includes((username).charAt(0))) {
         alert("Username phải trên 6 kí tự và bắt đầu bằng 1 kí tự chữ")
         return false;
     } else {
@@ -37,16 +36,16 @@ function Usernamecheck() {
     }
 }
 
-function Passwordcheck(str) {
-    if (/[a-z]/.test(str) && /[A-Z]/.test(str) && /[^a-zA-Z0-9\s]/.test(str)===false&&str.length>=8&&/[0-9]/.test(str)) {
+function Passwordcheck() {
+    if (/[a-z]/.test(password) && /[A-Z]/.test(password) && /[^a-zA-Z0-9\s]/.test(password)===false&&password.length>=8&&/[0-9]/.test(password)) {
         return true;
-    } else if (str.length<8) {
+    } else if (password.length<8) {
         alert("Mật khẩu phải có ít nhất 8 kí tự")
-    } else if ((/[a-z]/.test(str) && /[A-Z]/.test(str))!=true) {
+    } else if ((/[a-z]/.test(password) && /[A-Z]/.test(password))!=true) {
         alert("Mật khẩu phải chứa cả chữ thường và chữ hoa \n ví dụ: AbcdEfgH123456")
-    } else if (/[0-9]/.test(str)!=true) {
+    } else if (/[0-9]/.test(password)!=true) {
         alert("Mật khẩu phải chứa ít nhất 1 chữ số")
-    } else if (/[^a-zA-Z0-9\s]/.test(str)===true) {
+    } else if (/[^a-zA-Z0-9\s]/.test(password)===true) {
         alert("Mật khẩu không được chứa kí tự đặc biệt (Các kí tự nằm ngoài các chữ cái từ a - z, các chữ in hoa và chữ số)")
     }
 }
@@ -58,17 +57,30 @@ function Birthcheck() {
         return true;
     }
 }
-
-function SignUp() {
-    if (Usernamecheck()&&Passwordcheck(Password.value)&&Emailcheck()&&Birthcheck()) {
-        let User= {
-            "Email":Email.value,
-            "Username":Username.value,
-            "Password":Password.value,
-            "Gender":Gendercheck(),
-            "Date": date.value,
+if (Usernamecheck()&&Passwordcheck()&&Emailcheck()&&Birthcheck()) {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(Auth, email, password);
+        const { user} =userCredential;
+        localStorage.setItem("token", user.accessToken);
+        try {
+            const docref = await addDoc(collection(dbFireStore,"users"),{
+                username:username,
+                email:email,
+                id:user.uid,
+                date:date,
+                gender:Gendercheck()
+            })
+        } catch (error) {
+            console.error(error);
         }
-        localStorage.setItem("User",JSON.stringify(User));
-        window.location.href="../Login/login.html";
+     } catch (error) {
+        console.error(error);
+     }
+     window.location="../Login/login.html"
     }
 }
+form.addEventListener("submit", async (e)=> {
+    registerUser(e);
+})
+console.log(Auth,form);
+
