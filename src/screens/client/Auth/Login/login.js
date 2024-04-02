364d7115c4f0099e7a6725f265b3fa7d6dc6c708
mlemmlem-teaser <masteryibm@gmail.com>
2024-04-02 21:59:49 +0700

    gákdnasjbfhad

--- src/screens/client/Auth/Login/login.js
+++ src/screens/client/Auth/Login/login.js
@@ -1,30 +1,28 @@
-// const Username =document.getElementById("form2Example3");
-// const Email =document.getElementById("form2Example1");
-// const Password =document.getElementById("form2Example2");
-// function SignIn() {
-//     const user = JSON.parse(localStorage.getItem("User"));
-//     if (!user) {
-//       alert("user chưa có");
-//     } else {
-//       if (
-//         user.Email == Email.value &&
-//         user.Password == Password.value
-//       ) {
-//         window.location.href = "../../Homepage/homepage.html";
-//       } else {
-//         console.log(user)
-//         console.log("error");
-//       }
-//     }
-//   }
-const form = document.querySelector(".form_login");
- async function LoginUser(e) {
- e.preventDefault();
- const { target } = e;
- const email = target.email.value;
- const password =target.password.value;
- const username = target.username.value;
- const date = target.date.value;
- const gender = target.gender.value;
- console.log(email,password,username,date,gender); 
-}
+import { Auth, dbFireStore } from "../../../../config-firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const formLogin = document.querySelector(".form_login");
+
+const loginUser = async (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const email = target.email.value;
+  const password = target.password.value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      Auth,
+      email,
+      password
+    );
+    const { user } = userCredential;
+    localStorage.setItem("token", user.accessToken);
+    if (user.accessToken) {
+      window.location.href = "../../Homepage/homepage.html";
+    }
+  } catch (error) {
+    alert("Login failed");
+  }
+};
+
+formLogin.addEventListener("submit", async (e) => await loginUser(e));
---
+++ src/screens/client/Homepage/homepage.js
@@ -0,0 +1,58 @@
+//Phần login
+import { dbFireStore, Auth } from "../../../config-firebase.js";
+import { collection, getDocs, getDoc, doc,} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+const name_user = document.querySelector(".name_user");
+const token = localStorage.getItem("token");
+
+if (!token) {
+  window.location.href = "../Auth/Login/login.html";
+}
+const logout = () => {
+  localStorage.removeItem("token");
+  window.location.href = "../Auth/Login/login.html";
+};
+onAuthStateChanged(Auth, async (user) => {
+  if (user) {
+    const uid = user.uid;
+    const docRef = doc(dbFireStore, "users", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      name_user.innerText = data.name;
+    } else {
+      console.log("No such document!");
+    }
+  } else {
+  }
+});
+//Phần giao diện
+
+//Animation Sidebar 
+// function RunSideBar() {
+//   const SideBar = document.getElementById("sidebar");
+//   if (SideBar.style.display="flex") {
+//     SideBar.style.animation="LeftToRight 1s";
+//   } else {
+//     SideBar.style.animation="RightToLeft 1s";
+//   }
+// }
+// document.getElementById("MenuBtn").addEventListener("click",RunSideBar());
+document.addEventListener('DOMContentLoaded', function () {
+  const menuBtn = document.getElementById('MenuBtn');
+  const sidebar = document.getElementById('sidebar');
+
+  menuBtn.addEventListener('click', function () {
+      if (sidebar.style.display === 'none') {
+          sidebar.style.display = 'flex';
+          setTimeout(()=>{
+            sidebar.style.transform = 'translateX(0)';
+        },500)
+      } else {
+          sidebar.style.transform = 'translateX(-20vw)';
+          setTimeout(() => {
+              sidebar.style.display = 'none';
+          }, 500);
+      }
+  });
+});
