3073b930b48c6cf55603fdee5706cd86f279ff8d
mlemmlem-teaser <masteryibm@gmail.com>
2024-05-12 13:10:00 +0700

    ds

--- src/screens/admin/manageProduct/manageProduct.js
+++ src/screens/admin/manageProduct/manageProduct.js
@@ -1,4 +1,9 @@
-import { getDatabase, set, ref, get,} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 import { dbRealTime } from "../../../config-firebase.js";
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -20,6 +25,41 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//table
+async function getAllProduct() {
+  const data = await get(ref(dbRealTime, "products/"));
+  const arrayProduct = Object.keys(data.val()).map((uuid) => {
+    const objectProduct = { uuid };
+    Object.assign(objectProduct, data.val()[uuid]);
+    return objectProduct;
+  });
+  return arrayProduct;
+}
+let ProductInformation = document.getElementById("ProductInformation");
+function resetTable() {
+getAllProduct().then((items) => {
+  items.forEach((item) => {
+    console.log(item);
+    const modal = `
+  <tr>
+  <th class="padding">${item.nameProduct}</th>
+  <th class="number padding">${item.price} $</th>
+  <th>${item.description}</th>
+  <th class="textcenter"><label class="switch">
+  <input type="checkbox" checked>
+  <span class="slider round"></span>
+  </label>
+  </th>
+  <th class="number textcenter">999</th>
+  <th class="number padding">${item.price * 999} $</th>
+</tr>
+`;
+    ProductInformation.innerHTML += modal;
+  });
+});
+};
+resetTable();
 //uuid
 function generateUUID() {
   const timestamp = new Date().getTime();
@@ -30,7 +70,6 @@ function generateUUID() {
 //addProduct
 const addProduct = document.getElementById("small-button");
 const productForm = document.getElementById("productForm");
-console.log(addProduct, productForm);
 function openForm() {
   productForm.style.display = "inherit";
 }
@@ -56,4 +95,5 @@ saveBtn.addEventListener("click", async (e) => {
     description: description.value,
   });
   productForm.style.display = "none";
+  resetTable();
 });
