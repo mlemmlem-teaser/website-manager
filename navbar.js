7b9594f7f3f9e22ee0d74b3613255e2cf99c080c
mlemmlem-teaser <masteryibm@gmail.com>
2024-05-30 13:41:46 +0700

    dsd

--- navbar.js
+++ navbar.js
@@ -178,5 +178,26 @@ Avatar
     }
   });
 }
+
+
+//sidebar
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.getElementById("MenuBtn");
+  const sidebar = document.getElementById("sidebar");
+
+  menuBtn.addEventListener("click", function () {
+    if (sidebar.style.display === "none") {
+      sidebar.style.display = "flex";
+      setTimeout(() => {
+        sidebar.style.transform = "translateX(0)";
+      }, 200);
+    } else {
+      sidebar.style.transform = "translateX(-30vw)";
+      setTimeout(() => {
+        sidebar.style.display = "none";
+      }, 200);
+    }
+  });
+});
 resetState();
 export { resetState };
